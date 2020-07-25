ef40c81490805c3a304edda60b53ff567bd43dbf
robertramosastudillo <robertramosastudillo@gmail.com>
2020-07-25 15:15:35 -0500

    Primer commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import Server from './server/serve';
+import router from './router/router';
+import MySQL from './mysql/mysql';
+
+const server = Server.init(3000);
+server.app.use(router);
+
+// MySQL instance
+// MySQL.instance;
+
+server.start(() => {
+    console.log("Servidor corriendo en el puerto 3000");
+});
---
+++ src/mysql/mysql.ts
@@ -0,0 +1,55 @@
+import mysql = require("mysql");
+
+export default class MySQL {
+
+  private static _instance: MySQL;
+
+  cnn: mysql.Connection;
+  conectado: boolean = false;
+
+  constructor() {
+    console.log("Clase inicializada");
+
+    this.cnn = mysql.createConnection({
+      host: "localhost",
+      user: "node_user",
+      password: "12345",
+      database: "node_db",
+    });
+
+    this.conectarDB();
+  }
+
+  public static get instance(){
+    return this._instance || (this._instance = new this());
+  }
+
+  static ejecutarQuery(query: string, callback: Function){
+
+      this.instance.cnn.query(query,(err, results: Object[], fileds) => {
+        if(err){
+            console.log('Error en query');
+            console.log(err);
+            return callback(err);
+        }
+
+        if(results.length === 0){
+            callback('El registro solicitado no existe');
+        }else{
+            callback(null, results);
+        }
+      });
+  }
+
+  private conectarDB() {
+    this.cnn.connect((err: mysql.MysqlError) => {
+      if (err) {
+        console.log(err.message);
+        return;
+      } else {
+        this.conectado = true;
+        console.log("Base de datos online!");
+      }
+    });
+  }
+}
